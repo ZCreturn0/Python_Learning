e9d2878272f220f25d14d87c0db7708ad22ada35
ZCreturn0 <237350543@qq.com>
2018-08-28 22:46:55 +0800

    add attr,method in class;how to implement private,static,typeof in python

--- learn2.py
+++ learn2.py
@@ -3,11 +3,15 @@
 
 #定义学生类
 class Student(object):
+    #类属性
+    likeStatic = 'static'
+    count = 0
     #初始化
     def __init__(self,name,score):
         #__开头表示私有变量,但同时__表示特殊变量,外部可访问
         self.__name = name
         self.__score = score
+        Student.count += 1
     
     #输出
     def print_score(self):
@@ -15,3 +19,29 @@ class Student(object):
 
 a = Student('aa',69)
 a.print_score()
+
+#调用类属性
+print(a.likeStatic)
+print(Student.likeStatic)
+
+#创建实例自己的属性
+a.likeStatic = 'a_static'
+
+#a的属性比类属性级别高,覆盖类属性
+print(a.likeStatic)
+#类属性
+print(Student.likeStatic)
+
+#删除自身属性
+del a.likeStatic
+#输出类属性
+print(a.likeStatic)
+
+print(Student.count)
+b = Student('bb',69)
+c = Student('cc',69)
+d = Student('dd',69)
+print(Student.count)
+
+#print(type(a))
+#print(dir(a))
