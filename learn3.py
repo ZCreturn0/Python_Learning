21fd69392b81678a5c47480c176db5b539dff2de
ZCreturn0 <237350543@qq.com>
2018-09-02 23:15:13 +0800

    add object to json

--- learn3.py
+++ learn3.py
@@ -102,14 +102,43 @@ logging.basicConfig(level=logging.INFO)
 # pickle.dump(D,f)
 # f.close()
 
-F = open('pickle.txt','rb')
-l = pickle.load(F)
-F.close()
-d = pickle.loads(l)
-print(d)
-#dict转为json对象
-j = json.dumps(d)
-print(d)
-#json对象转为dict
-p = json.loads(j)
-print(p)
+# F = open('pickle.txt','rb')
+# l = pickle.load(F)
+# F.close()
+# d = pickle.loads(l)
+# print(d)
+# #dict转为json对象
+# j = json.dumps(d)
+# print(d)
+# #json对象转为dict
+# p = json.loads(j)
+# print(p)
+
+# #对象转为json
+# class Stu(object):
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+# #非类方法,不能为私有属性
+# def forDumps(dmp):
+#     return {
+#         'name':dmp.name,
+#         'age':dmp.age
+#     }
+# def forObj(dmp):
+#     return Stu(dmp['name'],dmp['age'])
+# s = Stu('aa',12)
+# # j = json.dumps(s,default=forDumps)
+# # print(j)
+# #__dict__:属性写成键值对
+# j = json.dumps(s,default=lambda obj: obj.__dict__)
+# print(j)
+
+# #json转为对象
+# o = json.loads(j,object_hook=forObj)
+# print(o)
+
+#对中文进行JSON序列化,True:二进制,False:中文
+obj = dict(name='小明', age=20)
+s = json.dumps(obj, ensure_ascii=True)
+print(s)
