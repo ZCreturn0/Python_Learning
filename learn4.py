a0ebe97c5a53f2518853204faca5fdea816c3ee8
ZCreturn0 <237350543@qq.com>
2018-09-03 20:52:59 +0800

    add Pool

--- learn4.py
+++ learn4.py
@@ -1,5 +1,5 @@
-from multiprocessing import Process
-import os,time.random
+from multiprocessing import Process,Pool
+import os,time,random
 
 # def child_func(arg):
 #     print('arg:%s   pid:%s' % (arg,os.getpid()))
@@ -13,10 +13,25 @@ import os,time.random
 #     p.join()
 #     print('child end')
 
-def multiPrecessingCallback(num):
-    print('task%s,pid:%s' % (num,os.getpid()))
-    strat = time.time()
-    time.sleep(random.random() * 3)
-    end = time.time()
-    print('task%s runs in %.2fs' % (num,end-start))
+# def multiPrecessingCallback(num):
+#     print('task%s,pid:%s' % (num,os.getpid()))
+#     start = time.time()
+#     time.sleep(random.random() * 3)
+#     end = time.time()
+#     print('task%s runs in %.2fs' % (num,end-start))
+
+# if __name__ == '__main__':
+#     print('parent:%s' % os.getpid())
+#     #建立进程池,最多跑4个进程(默认个数为CPU个数)
+#     p = Pool(4)
+#     for  i in range(5):
+#         p.apply_async(multiPrecessingCallback,args=(i,))
+#     print('wait for running:')
+#     #close后不能再添加进程
+#     p.close()
+#     #join等待进程执行完毕
+#     p.join()
+#     print('now,over')
+
+
 
