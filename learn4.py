be590d6c15efbd42499059c80ec4f0920b3e1f83
zcreturn0 <237350543@qq.com>
2018-09-03 17:58:43 +0800

    add function multiPrecessingCallback

--- learn4.py
+++ learn4.py
@@ -1,14 +1,22 @@
 from multiprocessing import Process
-import os
+import os,time.random
 
-def child_func(arg):
-    print('arg:%s   pid:%s' % (arg,os.getpid()))
+# def child_func(arg):
+#     print('arg:%s   pid:%s' % (arg,os.getpid()))
 
-if __name__ == '__main__':
-    print("parent:%s" % os.getpid())
-    #创建子进程
-    p = Process(target=child_func,args=('child',))
-    print('child start')
-    p.start()
-    p.join()
-    print('child end')
+# if __name__ == '__main__':
+#     print("parent:%s" % os.getpid())
+#     #创建子进程
+#     p = Process(target=child_func,args=('child',))
+#     print('child start')
+#     p.start()
+#     p.join()
+#     print('child end')
+
+def multiPrecessingCallback(num):
+    print('task%s,pid:%s' % (num,os.getpid()))
+    strat = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('task%s runs in %.2fs' % (num,end-start))
+
