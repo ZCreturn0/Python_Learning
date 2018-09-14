ec072ee307cbc4649b75647217ae16a7de45a072
zcreturn0 <237350543@qq.com>
2018-09-14 16:27:15 +0800

    one way to parse HTML

--- learn6.py
+++ learn6.py
@@ -27,30 +27,60 @@
 # parser.EndElementHandler = handler.end_element
 # parser.Parse(xml)
 
-from html.parser import HTMLParser
-from html.entities import name2codepoint
+# from html.parser import HTMLParser
+# from html.entities import name2codepoint
 
-class MyHTMLParser(HTMLParser):
-    #<div>
-    def handle_starttag(self,tag,attrs):
-        print("<%s>-----attrs:%s" % (tag,attrs))
+# class MyHTMLParser(HTMLParser):
+#     #<div>
+#     def handle_starttag(self,tag,attrs):
+#         print("handle_starttag--><%s>-----attrs:%s" % (tag,attrs))
     
-    #</div>
-    def handle_endtag(self,tag):
-        print("<%s/>" % (tag))
+#     #</div>
+#     def handle_endtag(self,tag):
+#         print("handle_endtag--><%s/>" % (tag))
 
-    #<image/>
-    def start_endtag(self,tag,attrs):
-        print("tag:<%s>    attrs:%s" % (tag,attrs))
+#     #<image/>
+#     def start_endtag(self,tag,attrs):
+#         print("start_endtag-->tag:<%s>    attrs:%s" % (tag,attrs))
 
-    #<..>   xxxxxxxxxxx   <..>
-    def handle_data(self,data):
-        print("data:%s" % (data))
+#     #<..>   xxxxxxxxxxx   <..>
+#     def handle_data(self,data):
+#         print("handle_data-->data:%s" % (data))
 
-    #<!-- -->
-    def handle_comment(self,text):
-        print("comment:%s" % (text))
+#     #<!-- -->
+#     def handle_comment(self,text):
+#         print("handle_comment-->comment:%s" % (text))
 
-html = r'''<div class="aaa"><img src="./aa/bb.jpg"/><span>密码:</span><input type="password"/></div>'''
-parse = MyHTMLParser()
-parse.feed(html)
+# html = r'''<div class="aaa"><img src="./aa/bb.jpg"/><span>密码:</span><input type="password"/></div>'''
+# parse = MyHTMLParser()
+# parse.feed(html)
+
+from pyquery import PyQuery as pq
+from urllib import request
+
+#会议对象
+class Meeting(object):
+    def __init__(self,year,when,where,name):
+        self.year = year
+        self.when = when
+        self.where = where
+        self.name = name
+
+meetingList = []
+
+html = pq(url='https://www.python.org/events/python-events/')
+ul = html(".list-recent-events")
+for i in range(0,ul.find("li").length):
+    year = ul.find("li").eq(i).find("p time .say-no-more").text()
+    when = ul.find("li").eq(i).find("p time").text()
+    where = ul.find("li").eq(i).find("p .event-location").text()
+    name = ul.find("li").eq(i).find(".event-title a").text()
+    m = Meeting(year,when,where,name)
+    meetingList.append(m)
+
+for item in meetingList:
+    print("年份:%s" % (item.year))
+    print("时间:%s" % (item.when))
+    print("地点:%s" % (item.where))
+    print("会议名称:%s" % (item.name))
+    print('\n\n')
