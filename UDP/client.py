b3902433e1676724f1c225349fe1a930821125fd
zcreturn0 <237350543@qq.com>
2018-09-17 14:18:28 +0800

    add comments

--- UDP/client.py
+++ UDP/client.py
@@ -6,6 +6,7 @@ import socket,time
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.sendto(b'aaaaa',('127.0.0.1',8888))
 time.sleep(2)
+#客户端recv
 print(s.recv(1024).decode('utf-8'))
 time.sleep(2)
 s.close()
--- UDP/server.py
+++ UDP/server.py
@@ -6,6 +6,7 @@ import socket
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind(('127.0.0.1',8888))
 while True:
+    #服务端recvfrom
     data,addr = s.recvfrom(1024)
     print('接收来自%s:%s的数据' % addr)
     s.sendto(b'%s?????' % data,addr)
